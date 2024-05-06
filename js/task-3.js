275c4315fcdc794100fcf935fe7d344140d041db
victoriaovcharova <victoria.ovcharova93@gmail.com>
2024-05-06 23:41:14 +0300

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,47 +1,44 @@
-
-const sortByDescendingFriendCount = (users) => {
-  return users.sort((a, b) => b.friends.length - a.friends.length);
-};
-
-
+'use strict';
+const sortByDescendingFriendCount = users =>
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 console.log(
   sortByDescendingFriendCount([
     {
-      name: "Moore Hensley",
-      friends: ["Sharron Pace"],
-      gender: "male"
+      name: 'Moore Hensley',
+      friends: ['Sharron Pace'],
+      gender: 'male',
     },
     {
-      name: "Sharlene Bush",
-      friends: ["Briana Decker", "Sharron Pace"],
-      gender: "female"
+      name: 'Sharlene Bush',
+      friends: ['Briana Decker', 'Sharron Pace'],
+      gender: 'female',
     },
     {
-      name: "Ross Vazquez",
-      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-      gender: "male"
+      name: 'Ross Vazquez',
+      friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+      gender: 'male',
     },
     {
-      name: "Elma Head",
-      friends: ["Goldie Gentry", "Aisha Tran"],
-      gender: "female"
+      name: 'Elma Head',
+      friends: ['Goldie Gentry', 'Aisha Tran'],
+      gender: 'female',
     },
     {
-      name: "Carey Barr",
-      friends: ["Jordan Sampson", "Eddie Strong"],
-      gender: "male"
+      name: 'Carey Barr',
+      friends: ['Jordan Sampson', 'Eddie Strong'],
+      gender: 'male',
     },
     {
-      name: "Blackburn Dotson",
-      friends: ["Jacklyn Lucas", "Linda Chapman"],
-      gender: "male"
+      name: 'Blackburn Dotson',
+      friends: ['Jacklyn Lucas', 'Linda Chapman'],
+      gender: 'male',
     },
     {
-      name: "Sheree Anthony",
-      friends: ["Goldie Gentry", "Briana Decker"],
-      gender: "female"
-    }
-  ])
+      name: 'Sheree Anthony',
+      friends: ['Goldie Gentry', 'Briana Decker'],
+      gender: 'female',
+    },
+  ]),
 );
 // [
 //   {
